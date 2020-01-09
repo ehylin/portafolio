2ca4f76ccd16a0b0aa70e057c500cbbb2c194de5
ehylin <ehylinolezjua@gmail.com>
2020-01-08 21:33:10 -0300

    cambiando foto header

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,3 +1,21 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+$(function(){
+
+  $('[data-toggle="popover"]').popover();
+
+  $(".nav-link").click(function(event){
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var mov = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(mov).offset().top
+      }, 600, function(){
+        window.location.hash = mov;
+      });
+    }
+  });
+
+
+
+});
